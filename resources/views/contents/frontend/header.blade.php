399688ffa70cc95ad6e2aeb8f200edbef2ca7cd9
Andreas Ek <andreas@andreasek.se>
2016-08-30 17:11:19 +0200

    Frontpage header adjusted

--- resources/views/contents/frontend/header.blade.php
+++ resources/views/contents/frontend/header.blade.php
@@ -3,11 +3,11 @@
 
         <div class="uk-vertical-align uk-text-center" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 450px;">
             <div class="uk-vertical-align-middle uk-width-1-2">
-                <h1 class="uk-heading-large">Sample Heading</h1>
-                <p class="uk-text-large">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo.</p>
+                <h1 class="uk-heading-large">Laravel with uikit</h1>
+                <p class="uk-text-large">Example layouts with uikit implemented in the latest version of Laravel 5.3 as a project starter.</p>
                 <p>
-                    <a class="uk-button uk-button-primary uk-button-large" href="#">Button</a>
-                    <a class="uk-button uk-button-large" href="#">Button</a>
+                    <a class="uk-button uk-button-primary uk-button-large" href="https://github.com/ekandreas/l35uikit"><i class="uk-icon-bug"></i> Github</a>
+                    <a class="uk-button uk-button-large" href="http://getuikit.com/docs/layouts_frontpage.html"><i class="uk-icon-code"></i> Raw example layouts at uikit</a>
                 </p>
             </div>
         </div>
